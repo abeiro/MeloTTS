636b090f6190a23a1306362d15a5c974f7744445
Augusto Beiro <augusto.beiro@gmail.com>
2024-09-30 10:43:30 +0200

    * Will download model from hf

--- melo/restapi.py
+++ melo/restapi.py
@@ -1,34 +1,45 @@
-from fastapi import FastAPI, HTTPException, File, UploadFile
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse, JSONResponse
 import os, torch, io
 from melo.api import TTS
 import time
+from huggingface_hub import hf_hub_download
 
-# Initialize FastAPI restapi
+# Initialize FastAPI
 restapi = FastAPI()
+
+# Function to download the checkpoint and config from Hugging Face
+def download_hf_model():
+    # Download the checkpoint.pth file
+    ckpt_path = hf_hub_download(repo_id="tylermaister/SkyrimLikeVoices", filename="checkpoint.pth")
+
+    # Download the config.json file
+    custom_config_path = hf_hub_download(repo_id="tylermaister/SkyrimLikeVoices", filename="config.json")
+
+    return ckpt_path, custom_config_path
 
 # Load models at startup
 device = 'cpu'
-models = {
-    'EN': TTS(language='EN', device=device)
-}
+ckpt_path, custom_config_path = download_hf_model()
 
-ckpt_path = "data/SkyrimLikeVoices/checkpoint.pth"
-custom_config_path = os.path.join(os.path.dirname(ckpt_path), 'config.json')
+# Initialize the custom TTS model
 model_custom = TTS(language='EN', config_path=custom_config_path, ckpt_path=ckpt_path, device=device)
 
-# Add the custom model to the dictionary
-models['SkyrimLikeVoices'] = model_custom
+# Add the custom model to the models dictionary
+models = {
+    'EN': TTS(language='EN', device=device),
+    'SkyrimLikeVoices': model_custom
+}
 
 # Speaker IDs for each language
 speaker_ids = models['SkyrimLikeVoices'].hps.data.spk2id
-# print(speaker_ids.keys())
+
 # Function to synthesize the speech
 def synthesize(speaker, text, speed, language):
     try:
         start_time = time.time()
         bio = io.BytesIO()
-        
+
         # Run TTS
         models['SkyrimLikeVoices'].tts_to_file(
             text,
@@ -37,10 +48,10 @@ def synthesize(speaker, text, speed, language):
             speed=speed,
             format='wav'
         )
-        
+
         end_time = time.time()
         print(f"Synthesis completed in {end_time - start_time:.2f} seconds.")
-        
+
         # Return the audio file
         bio.seek(0)
         return bio
@@ -67,3 +78,14 @@ async def tts(speaker: str, text: str, speed: float = 1.0, language: str = 'EN')
     # Return the audio as a stream response
     return StreamingResponse(audio, media_type="audio/wav")
 
+
+@restapi.get("/tts")
+async def tts(speaker: str, text: str, speed: float = 1.0, language: str = 'EN'):
+    
+    if speaker not in models['SkyrimLikeVoices'].hps.data.spk2id:
+        raise HTTPException(status_code=400, detail="Invalid speaker specified")
+    
+    audio = synthesize(speaker, text, speed, 'EN')
+
+    # Return the audio as a stream response
+    return StreamingResponse(audio, media_type="audio/wav")
